ec095af20e6a072517e17b8ddc146d3ebcff8a16
dieterds <bytubed2@gmail.com>
2016-09-13 14:30:58 +0200

    #3
    possibility to copy only the videolinks to clipboard on videolinkpage
    (button at the top)

--- js/output.js
+++ js/output.js
@@ -1,14 +1,39 @@
-﻿$(document).ready(function() {
+﻿var copybuttonstring = "<p><div class=\"center\"><button id=\"copytoclipboard\"><img src=\"gfx/Start-16.png\"> Copy all links to clipboard</button></div><p/>";
+var sandboxstring = "<textarea hidden id=\"sandbox\"></textarea>";
+var statusstring = "<p hidden id=\"copystatus\" class=\"center\"></p>"
+$(document).ready(function() {
     var bgPage = chrome.extension.getBackgroundPage();
     var params = getParamsFromUrl(document.URL);
     if (params["data"] !== null && bgPage.data[params["data"]] !== undefined) {
-        this.body.innerHTML = bgPage.data[params["data"]];
+        this.body.innerHTML = copybuttonstring + statusstring + bgPage.data[params["data"]] + sandboxstring;
     }
     else{
         this.body.innerHTML =  "No data under this ID !";
     }
+    $('#copytoclipboard').click(function () {
+        var linksToCopy = "";
+
+        $('a[href*=http]').each(function (index, ele) {
+            linksToCopy += ele.href + "\n";
+        });
+        $('#sandbox').show();
+        try {
+            setClipboard(linksToCopy);
+            $('#copystatus').html("Successfully copied links to clipboard!");
+        } catch (err) {
+            $('#copystatus').html("Failed to copy links to clipboard!");
+        }
+        $('#sandbox').hide();
+        $('#copystatus').show();
+        $('#copystatus').fadeOut(3000);
+    });
 });
 
+function setClipboard(str) {
+    var sandbox = $('#sandbox').val(str).select();
+    document.execCommand('copy');
+    sandbox.val('');
+}
 
 function getParamsFromUrl(url) {
     try {
